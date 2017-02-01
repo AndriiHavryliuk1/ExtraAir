83d52c25ac2526a2484e7da2ada652c70bd70021
AndriiHavryliuk <andriy.gavrulyik@gmail.com>
2017-02-02 01:20:25 +0200

    first doc

---
+++ scr/WebApi/ExtaAirAPI/ExtaAirAPI/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ExtaAirAPI.Startup))]
+
+namespace ExtaAirAPI
+{
+	public partial class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			ConfigureAuth(app);
+		}
+	}
+}
---
+++ scr/WebApi/ExtaAirAPI/ExtraAirCore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExtraAirCore.WebApiApplication" Language="C#" %>
